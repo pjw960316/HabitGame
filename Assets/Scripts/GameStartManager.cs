e13388a32fc0b87b54c8428181b02b7091c27782
JIWON PARK <55792986+pjw960316@users.noreply.github.com>
2025-05-20 17:43:21 +0900

    etc. TODO 추가

--- Assets/Scripts/GameStartManager.cs
+++ Assets/Scripts/GameStartManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,6 +7,13 @@ using UnityEngine.SceneManagement;
  * loadScene에서 모든 동작을 완료했을 때 -> MainScene을 진입하도록
  * 모든 싱글턴을 초기화 시키기
  * 이건 Mono가 상속이 필요한가?
+ */
+
+/*
+ * TODO:
+ * GameStartMAnager에서 싱글턴을 모두 초기화 하는 걸 Awake랑 Start로 나누었다면
+ * Awake와 Start에서 정확히 어떤 걸 초기화하면 좋을 지 연습해서 Personal Coding Style에 적자.
+ *  예를 들면 Binding 같은건.
  */
 public class GameStartManager : MonoBehaviour
 {
@@ -17,6 +25,11 @@ public class GameStartManager : MonoBehaviour
         changeSceneManager.ChangeScene();
         
         DontDestroyOnLoad(this); //TODO : Should need? 너의 책임은 시작시키고 죽는건데?
+    }
+
+    private void Start()
+    {
+        
     }
 }
 
