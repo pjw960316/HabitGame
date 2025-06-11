2a91312fb47120294e4cfb99c1bb2d14d3711ece
JIWON PARK <55792986+pjw960316@users.noreply.github.com>
2025-06-11 12:54:28 +0900

    feat. data controller split

--- Assets/Scripts/Manager/MyCharacterManager.cs
+++ Assets/Scripts/Manager/MyCharacterManager.cs
@@ -1,13 +1,14 @@
 /* TODO
- 1. 내 캐릭터가 들고 있어야 할 정보.
- 
- */
+ 1. 내 캐릭터의 model인 _myCharacterData를 로드하고 이용한다.
+ 2. _myCharacterData를 이용해서 캐릭터의 동작을 관리한다.
+ 3. MyCharacterManager 보다 추상화 단계가 낮은 1개의 책임을 수행하는 여러 클래스 (Controller 분해)로 component 채우는. 
+*/
 public class MyCharacterManager : SingletonBase<MyCharacterManager>, IManager
 {
+    private MyCharacterData _myCharacterData;
     public MyCharacterManager()
     {
-        Budget = 0;
+        //TODO : data load -> GameStartManager에서 한 번에
+        
     }
-
-    public int Budget { get; private set; }
 }
