868d811ff11b110bb45a3927d3d612bda88fe827
JIWON PARK <55792986+pjw960316@users.noreply.github.com>
2025-07-22 20:54:49 +0900

    feat. ToastManager Test

---
+++ HabitGame/Assets/Script/Manager/UIToastManager.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIToastManager : ManagerBase<UIToastManager>, IManager, IDisposable
+{
+    #region 1. Fields
+
+    // default
+
+    #endregion
+
+    #region 2. Properties
+
+    // default
+
+    #endregion
+
+    #region 3. Constructor
+
+    public void Initialize()
+    {
+    }
+
+    #endregion
+
+    #region 4. Methods
+
+    public void SetModel(IEnumerable<ScriptableObject> _list)
+    {
+    }
+    
+    public void ConnectInstanceByActivator(IManager instance)
+    {
+        if (_instance == null)
+        {
+            _instance = instance as UIToastManager;
+        }
+    }
+    
+    #endregion
+
+    #region 5. EventHandlers
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion
+}
