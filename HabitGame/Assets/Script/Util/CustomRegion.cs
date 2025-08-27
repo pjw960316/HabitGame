8b132a1c0d10db049eca62d2c819daafbe85e4cc
JIWON PARK <55792986+pjw960316@users.noreply.github.com>
2025-08-27 12:09:13 +0900

    chore. custom REgion 개행

--- HabitGame/Assets/Script/Util/CustomRegion.cs
+++ HabitGame/Assets/Script/Util/CustomRegion.cs
@@ -1,26 +1,38 @@
 public class CustomRegion
 {
     #region 1. Fields
+
     //
+
     #endregion
 
     #region 2. Properties
+
     //
+
     #endregion
 
     #region 3. Constructor
+
     //
+
     #endregion
 
     #region 4. EventHandlers
+
     //
+
     #endregion
-    
+
     #region 5. Request Methods
+
     // 
+
     #endregion
 
     #region 6. Methods
+
     // 
+
     #endregion
 }
