a829b1ecaefdfb85e5e96683db0e1eb91751d886
JIWON PARK <55792986+pjw960316@users.noreply.github.com>
2025-08-28 10:49:43 +0900

    refactor. 1차 리팩터링

--- HabitGame/Assets/Script/Manager/XmlDataSerializeManager.cs
+++ HabitGame/Assets/Script/Manager/XmlDataSerializeManager.cs
@@ -1,20 +1,20 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
 using UnityEngine;
+
 
 // Note
 // 책임
 // 1. 모든 XML 데이터를 Deserialize 할 책임
 // 2. Deserialize 된 XML Data Instance를 각각의 Manager에게 전달.
 // 3. Serialize를 통해 XML Data를 업데이트
-public class XmlDataSerializeManager : ManagerBase<XmlDataSerializeManager>, IManager
+public partial class XmlDataSerializeManager : ManagerBase<XmlDataSerializeManager>, IManager
 {
     #region 1. Fields
-    
-    private Dictionary<Type, string> _xmlFullPathDictionary;
+
     private List<IModel> _modelList;
+    private List<XmlFileData> _xmlFileDataList;
 
     #endregion
 
@@ -28,45 +28,69 @@ public class XmlDataSerializeManager : ManagerBase<XmlDataSerializeManager>, IMa
 
     public void PreInitialize()
     {
-        _xmlFullPathDictionary = new Dictionary<Type, string>();
         _modelList = new List<IModel>();
+        _xmlFileDataList = new List<XmlFileData>();
 
+        InitializeXmlFileDataList();
         Test1();
-        
-        //SetXmlFullPath();
     }
 
     public void Initialize()
     {
         //
-        
     }
 
     #endregion
 
-    #region 4. Methods
+    #region 4. EventHandlers
+
+    //
+
+    #endregion
+
+    #region 5. Request Methods
+
+    // 
+
+    #endregion
+
+    #region 6. Methods
 
     public void SetModel(IEnumerable<IModel> _list)
     {
     }
 
-    private void SetXmlFullPath()
+    public List<IModel> GetModelListWithDeserializedXml()
     {
-        //test
-        var path = Path.Combine(Application.dataPath, "Resources/MyCharacterData.xml");
-        _xmlFullPathDictionary.Add(typeof(MyCharacterData), path);
+        return _modelList;
     }
 
-    private void SetModelListWithDeserializedXml()
+    private void Test1()
     {
-        foreach (var element in _xmlFullPathDictionary)
+        foreach (var xmlFileData in _xmlFileDataList)
         {
-            var xmlType = element.Key;
-            var xmlPath = element.Value;
+            var xmlType = xmlFileData.DataType;
+            var xmlRelativePath = xmlFileData.RelativePath;
+            var xmlAbsolutePath = xmlFileData.AbsolutePath;
 
-            var xmlText = GetAllText(xmlPath);
+            var text = "";
+            if (!File.Exists(xmlAbsolutePath))
+            {
+                text = Resources.Load<TextAsset>(xmlRelativePath).text;
+                File.WriteAllText(xmlAbsolutePath, text);
+            }
+            else
+            {
+                text = File.ReadAllText(xmlAbsolutePath);
+
+                if (text == null)
+                {
+                    throw new FileLoadException("Read Xml fail");
+                }
+            }
+
             var xmlSerializer = new XmlSerializer(xmlType);
-            var stringReader = new StringReader(xmlText);
+            var stringReader = new StringReader(text);
             ExceptionHelper.CheckNullException(stringReader, "stringReader");
 
             var model = xmlSerializer.Deserialize(stringReader) as IModel;
@@ -75,67 +99,15 @@ public class XmlDataSerializeManager : ManagerBase<XmlDataSerializeManager>, IMa
         }
     }
 
-    public List<IModel> GetModelListWithDeserializedXml()
-    {
-        return _modelList;
-    }
-    private void Test1()
-    {
-        if (!File.Exists(Application.persistentDataPath + "/MyCharacterData.xml"))
-        {
-            Debug.Log("없어");
-            var textAsset = Resources.Load<TextAsset>("MyCharacterData").text;
-
-            File.WriteAllText(Application.persistentDataPath + "/MyCharacterData.xml", textAsset);
-        }
-        else
-        {
-            Debug.Log("있어");
-            SetXmlFullPath();
-            SetModelListWithDeserializedXml();
-        }
-        
-        Debug.Log($"{Application.persistentDataPath + "/MyCharacterData.xml"}");
-    }
-    private string GetAllText(string resourceFullPath)
-    {
-        var text = File.ReadAllText(resourceFullPath);
-
-        if (text == null)
-        {
-            throw new FileLoadException("Read Xml fail");
-        }
-
-        return text;
-    }
-
     public void SerializeXmlData<TModel>(TModel model) where TModel : IModel
     {
         var serializer = new XmlSerializer(typeof(MyCharacterData));
-        var path = _xmlFullPathDictionary[typeof(MyCharacterData)];
+        var path = _xmlFileDataList[0].AbsolutePath; //test
 
         using var writer = new StreamWriter(path);
 
         serializer.Serialize(writer, model);
     }
-
-    // refactor 
-    // addressable
-    // Resources.Load는 Runtime에 갱신된 xml 데이터를 반영하지 못한다.
-    private TextAsset GetTextAsset(string resourcePath)
-    {
-        var textAsset = Resources.Load<TextAsset>(resourcePath);
-
-        ExceptionHelper.CheckNullException(textAsset, "textAsset");
-
-        return textAsset;
-    }
-
-    #endregion
-
-    #region 5. EventHandlers
-
-    // default
 
     #endregion
 }
