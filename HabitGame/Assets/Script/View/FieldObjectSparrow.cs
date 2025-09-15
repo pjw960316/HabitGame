2aaef79354869c7ba7536004e3e22a7413b4d797
JIWON PARK <55792986+pjw960316@users.noreply.github.com>
2025-09-15 12:37:31 +0900

    chore. repack prefab

--- HabitGame/Assets/Script/View/FieldObjectSparrow.cs
+++ HabitGame/Assets/Script/View/FieldObjectSparrow.cs
@@ -1,3 +1,51 @@
+using System;
+using UnityEngine;
+
 public class FieldObjectSparrow : FieldObjectBase
 {
+    #region 1. Fields
+
+    [SerializeField] private Transform _sparrowTransform;
+
+    #endregion
+
+    #region 2. Properties
+
+    //
+
+    #endregion
+
+    #region 3. Constructor
+
+    // refactor 
+    // base
+    private void Awake()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        _sparrowTransform.position += new Vector3(0.02f, 0, 0);
+    }
+
+    #endregion
+
+    #region 4. EventHandlers
+
+    //
+
+    #endregion
+
+    #region 5. Request Methods
+
+    // 
+
+    #endregion
+
+    #region 6. Methods
+
+    // 
+
+    #endregion
 }
