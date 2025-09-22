9d9af3e4fa094826a31c904c2284dc4a3c3d269e
JIWON PARK <55792986+pjw960316@users.noreply.github.com>
2025-09-22 11:31:05 +0900

    refactor. 로직을 위한 class + 코드 중복 개선
    현재 제대로 위치가 나타나지는 않지만 (간단한 버그 일 듯)
    기존에 잘 못하던 중복+로직분할을 해냈다.

--- HabitGame/Assets/Script/View/FieldObjectLand.cs
+++ HabitGame/Assets/Script/View/FieldObjectLand.cs
@@ -1,8 +1,44 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
+#pragma warning disable CS0414 // Field is assigned but its value is never used
+#pragma warning disable CS0219 // Variable is assigned but its value is never used
+
 public class FieldObjectLand : FieldObjectBase
 {
+    public enum EPath
+    {
+        Top,
+        Bottom,
+        Left,
+        Right
+    }
+    public class PlacementHelper
+    {
+        private int _offset;
+        private readonly Dictionary<EPath,Vector3> _placeVectorDictionary = new();
+        
+        public PlacementHelper(float environment_X_Length, float environment_Z_Length)
+        {
+            _placeVectorDictionary[EPath.Bottom] = new Vector3(environment_X_Length * _offset, 0, 0);
+            _placeVectorDictionary[EPath.Top] = new Vector3(environment_X_Length * _offset, 0, environment_X_Length * SQUARE_SIDE_COUNT);
+            _placeVectorDictionary[EPath.Left] = new Vector3(0, 0, environment_Z_Length * _offset);
+            _placeVectorDictionary[EPath.Right] = new Vector3(environment_Z_Length * SQUARE_SIDE_COUNT, 0, environment_Z_Length * _offset);
+        }
+
+        public Vector3 GetPosition(EPath path, int offset)
+        {
+            _offset = offset;
+            if (_placeVectorDictionary.TryGetValue(path, out var pos))
+            {
+                return pos;
+            }
+
+            throw new KeyNotFoundException();
+        }
+    }
+    
     #region 1. Fields
 
     [SerializeField] private GameObject _treePrefab;
@@ -11,7 +47,17 @@ public class FieldObjectLand : FieldObjectBase
     [SerializeField] private GameObject _bushPrefab;
     [SerializeField] private Transform _environmentsBaseTransform;
 
-    private FieldObjectSparrow _fieldObjectSparrow;
+    private readonly int _borderEnvironmentsCount = 100;
+    private const int SQUARE_SIDE_COUNT = 4;
+    private int _oneSideEnvironmentCount;
+
+    private readonly List<EPath> _createOrder = new()
+    {
+        EPath.Bottom,
+        EPath.Top,
+        EPath.Left,
+        EPath.Right
+    };
 
     #endregion
 
@@ -27,13 +73,14 @@ public class FieldObjectLand : FieldObjectBase
     {
         base.OnStart();
 
-        SetFieldObjectSparrow();
         CreateFieldObjectEnvironments();
     }
 
     protected sealed override void Initialize()
     {
         base.Initialize();
+        
+        _oneSideEnvironmentCount = _borderEnvironmentsCount / SQUARE_SIDE_COUNT;
     }
 
     protected override void InitializeEnumFieldObjectKey()
@@ -45,52 +92,35 @@ public class FieldObjectLand : FieldObjectBase
     {
     }
 
-    private void SetFieldObjectSparrow()
-    {
-        _fieldObjectSparrow = _fieldObjectManager.GetFieldObject<FieldObjectSparrow>(EFieldObject.SPARROW);
-    }
-
     private void CreateFieldObjectEnvironments()
     {
-        // refactor
-        List<GameObject> list = new List<GameObject>();
-        for (int i = 0; i < 100; i++)
+        var list = new List<Transform>();
+        for (var i = 0; i < _borderEnvironmentsCount; i++)
         {
-            list.Add(Instantiate(_rockPrefab, _environmentsBaseTransform));
-        }
-        
-        var x = list[0].GetComponent<FieldObjectEnvironmentBase>().GetEnvironment_X_Length();
-        var z = list[0].GetComponent<FieldObjectEnvironmentBase>().GetEnvironment_Z_Length();
-        
-
-        int idx = 0;
-        for (int i = 0; i < 25; i++)
-        {
-            list[i].transform.position = new Vector3(x * idx, 0, 0) + _environmentsBaseTransform.position;
-            idx++;
+            // note
+            // high cost
+            list.Add(Instantiate(_rockPrefab, _environmentsBaseTransform).transform);
         }
 
-        idx = 0;
-        for (int i = 25; i < 50; i++)
-        {
-            list[i].transform.position = new Vector3(x * idx, 0, x * 25) + _environmentsBaseTransform.position;
-            idx++;
-        }
+        var firstObject = list[0].GetComponent<FieldObjectEnvironmentBase>();
+        var environment_X_Length = firstObject.GetEnvironment_X_Length();
+        var environment_Z_Length = firstObject.GetEnvironment_Z_Length();
 
-        idx = 0;
-        for (int i = 50; i < 75; i++)
-        {
-            list[i].transform.position = new Vector3(0, 0, z * idx) + _environmentsBaseTransform.position;
-            idx++;
-        }
+        var placementHelper = new PlacementHelper(environment_X_Length, environment_Z_Length);
+        var offset = 0;
 
-        idx = 0;
-        for (int i = 75; i < 100; i++)
+        for (var i = 0; i < SQUARE_SIDE_COUNT; i++)
         {
-            list[i].transform.position = new Vector3(z * 25, 0, z * idx) + _environmentsBaseTransform.position;
-            idx++;
+            for (var j = 0; j < _oneSideEnvironmentCount; j++)
+            {
+                var idx = i * j;
+                list[idx].position = placementHelper.GetPosition(_createOrder[i], offset) +
+                                     _environmentsBaseTransform.position;
+                offset++;
+            }
+
+            offset = 0;
         }
-       
     }
 
     #endregion
