2ab123b90b978b546bb614dfa75b3ed8bcf3c5c2
JIWON PARK <55792986+pjw960316@users.noreply.github.com>
2025-10-15 13:29:32 +0900

    feat. 카메라 해상도에 맞게 FOV 대응 + 하드 코딩 수정

--- HabitGame/Assets/Script/Controller/MainCameraMono.cs
+++ HabitGame/Assets/Script/Controller/MainCameraMono.cs
@@ -7,8 +7,12 @@ public class MainCameraMono : MonoBehaviour
 {
     #region 1. Fields
 
+    private const float UNITY_DEV_ASPECT_RATIO = 1080f / 1920;
+    private const float FOLLOWING_CAMERA_FOV = 86f;
+    private const float FOLLOWING_CAMERA_UPDATE_MILLISECONDS = 10f;
+
     [SerializeField] private Camera _mainCamera;
-    
+
     private CameraManager _cameraManager;
     private Transform _mainCameraTransform;
     private IDisposable _followFieldObjectObservable;
@@ -16,7 +20,10 @@ public class MainCameraMono : MonoBehaviour
     private Vector3 _initializedMainCameraPosition;
     private Quaternion _initializedMainCameraRotation;
     private float _initializedMainCameraFOV;
-    
+
+    private readonly Vector3 FOLLOWING_CAMERA_ROTATE_ADJUST_VECTOR = new(0, -0.7f, -2);
+    private readonly Vector3 FOLLOWING_CAMERA_POSITION_ADJUST_VECTOR = new(0, 1, -1);
+
     #endregion
 
     #region 2. Properties
@@ -30,7 +37,7 @@ public class MainCameraMono : MonoBehaviour
     private void Awake()
     {
         Initialize();
-        
+
         _cameraManager.SetMainCamera(this);
     }
 
@@ -39,19 +46,30 @@ public class MainCameraMono : MonoBehaviour
         _cameraManager = CameraManager.Instance;
         _mainCameraTransform = _mainCamera.transform;
 
-        InitializeCamera();
+        InitializeCameraFOV();
+
         CacheInitializedCameraData();
     }
 
-    private void InitializeCamera()
+    private void InitializeCameraFOV()
     {
-        
+        // note : 개발 단계에서 기획자가 최선의 각도를 맞춰 놓았을 것.
+        var originFOVDegree = _mainCamera.fieldOfView;
+        var originTanFOV = Mathf.Tan(originFOVDegree * Mathf.Deg2Rad / 2f);
+
+        var deviceAspect = Screen.width / (float)Screen.height;
+        var aspectRatio = UNITY_DEV_ASPECT_RATIO / deviceAspect;
+
+        var newFovDegree = 2f * Mathf.Atan(originTanFOV * aspectRatio) * Mathf.Rad2Deg;
+
+        _mainCamera.fieldOfView = newFovDegree;
+        _initializedMainCameraFOV = _mainCamera.fieldOfView;
     }
+
     private void CacheInitializedCameraData()
     {
         _initializedMainCameraPosition = _mainCameraTransform.position;
         _initializedMainCameraRotation = _mainCameraTransform.rotation;
-        _initializedMainCameraFOV = _mainCamera.fieldOfView;
     }
 
     #endregion
@@ -72,21 +90,23 @@ public class MainCameraMono : MonoBehaviour
 
     public void UpdateToFollowFieldObject(Transform fieldObjectTransform)
     {
-        //_mainCameraTransform.LookAt(fieldObjectTransform);
-        _mainCamera.fieldOfView = 86f;
-        
+        _mainCamera.fieldOfView = FOLLOWING_CAMERA_FOV;
+
         _followFieldObjectObservable?.Dispose();
-        _followFieldObjectObservable = Observable.Interval(TimeSpan.FromMilliseconds(10f)).Subscribe(_ =>
-        {
-            if(_mainCameraTransform == null)
+        _followFieldObjectObservable = Observable
+            .Interval(TimeSpan.FromMilliseconds(FOLLOWING_CAMERA_UPDATE_MILLISECONDS))
+            .Subscribe(_ =>
             {
-                return;
-            }
-            
-            Vector3 direction = fieldObjectTransform.position - new Vector3(0,-0.7f,-2) - _mainCameraTransform.position;
-            _mainCameraTransform.rotation = Quaternion.LookRotation(direction.normalized);
-            _mainCameraTransform.position = fieldObjectTransform.position + new Vector3(0, 1, -1);
-        });
+                if (_mainCameraTransform == null)
+                {
+                    return;
+                }
+
+                var direction = fieldObjectTransform.position - _mainCameraTransform.position -
+                                FOLLOWING_CAMERA_ROTATE_ADJUST_VECTOR;
+                _mainCameraTransform.rotation = Quaternion.LookRotation(direction.normalized);
+                _mainCameraTransform.position = fieldObjectTransform.position + FOLLOWING_CAMERA_POSITION_ADJUST_VECTOR;
+            });
     }
 
     public void DisposeFollowSparrowCameraMoving()
