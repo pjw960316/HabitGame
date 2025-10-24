eaaf8bab492ffa52a86bf3c701bb0dec6db170c0
JIWON PARK <55792986+pjw960316@users.noreply.github.com>
2025-10-24 16:24:33 +0900

    chore. 주석

--- HabitGame/Assets/Script/Manager/IManager.cs
+++ HabitGame/Assets/Script/Manager/IManager.cs
@@ -2,7 +2,10 @@ using System.Collections.Generic;
 
 public interface IManager
 {
+    // note : 외부 데이터 없이 스스로 온전히 초기화 가능한 것 구현
     public void PreInitialize();
+    
+    // note : 외부 데이터가 필요하거나, 시점이 빠르지 않아도 되는 것 구현
     public void Initialize();
     public void SetModel(); 
     public void BindEvent();
