f36c6a8c5b8a2e2ea0eb483d7ee932202a588c0e
JIWON PARK <55792986+pjw960316@users.noreply.github.com>
2025-10-29 13:32:25 +0900

    Update UIAlarmPopup.cs

--- HabitGame/Assets/Script/View/UI/UIAlarmPopup.cs
+++ HabitGame/Assets/Script/View/UI/UIAlarmPopup.cs
@@ -64,8 +64,7 @@ public class UIAlarmPopup : UIPopupBase
 
     protected sealed override void BindEvent()
     {
-        BindButtonMenuEvents(AlarmAudioClipButtons);
-        BindButtonMenuEvents(AlarmTimeButtons);
+        BindWidgetEvents();
 
         _confirmButton?.OnClick.AddListener(OnClickConfirmButton);
     }
@@ -87,6 +86,12 @@ public class UIAlarmPopup : UIPopupBase
 
     #region 4. EventHandlers
 
+    private void BindWidgetEvents()
+    {
+        BindButtonMenuEvents(AlarmAudioClipButtons);
+        BindButtonMenuEvents(AlarmTimeButtons);
+    }
+    
     private void BindButtonMenuEvents(List<UIAlarmButton> list)
     {
         foreach (var widget in list)
