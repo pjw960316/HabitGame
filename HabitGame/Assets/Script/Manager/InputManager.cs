3a807865c72471aa3801a900892cb88782df62dc
JIWON PARK <55792986+pjw960316@users.noreply.github.com>
2026-02-20 12:29:00 +0900

    feat. 계속 개발 -> 이번에는 voca

---
+++ HabitGame/Assets/Script/Manager/InputManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class InputManager : ManagerBase<InputManager>
+{
+    public class CustomRegion
+    {
+        #region 1. Fields
+
+        //
+
+        #endregion
+
+        #region 2. Properties
+
+        //
+
+        #endregion
+
+        #region 3. Constructor
+
+        //
+
+        #endregion
+
+        #region 4. EventHandlers
+
+        //
+
+        #endregion
+
+        #region 5. Request Methods
+
+        // 
+
+        #endregion
+
+        #region 6. Methods
+
+        // 
+
+        #endregion
+    }
+}
