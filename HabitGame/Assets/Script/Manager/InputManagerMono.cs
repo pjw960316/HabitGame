ac879ea555ab158d77b48fc9eb48d6f6d9a9bd7e
JIWON PARK <55792986+pjw960316@users.noreply.github.com>
2026-03-04 10:41:22 +0900

    feat. Input 체계 구현

--- HabitGame/Assets/Script/Manager/InputManagerMono.cs
+++ HabitGame/Assets/Script/Manager/InputManagerMono.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using AYellowpaper.SerializedCollections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,7 +10,12 @@ public class InputManagerMono : MonoBehaviour
     #region 1. Fields
 
     [SerializeField] private PlayerInput _playerInput;
-
+    
+    // InputActionReference 추가하면 Dictionary에 추가하세요
+    [SerializeField] private InputActionReference _moveInput;
+    
+    private readonly Dictionary<InputAction, Action<InputAction.CallbackContext>> _actionDict = new ();
+    
     #endregion
 
     #region 2. Properties
@@ -20,19 +28,47 @@ public class InputManagerMono : MonoBehaviour
 
     private void Awake()
     {
+        InitializeActionDictionary();
+
+        if (_playerInput.currentActionMap.Count() != _actionDict.Count)
+        {
+            Debug.LogError("PlayerInput Component의 액션 개수와 _actionDict의 액션 개수가 다르다.");
+        }
+        
         _playerInput.onActionTriggered += OnHandleInput;
     }
 
+    private void InitializeActionDictionary()
+    {
+        _actionDict[_moveInput.action] = OnHandleMove;
+    }
     #endregion
 
     #region 4. EventHandlers
-    
+
     private void OnHandleInput(InputAction.CallbackContext context)
     {
-        var inputValue = context.ReadValue<Vector2>();
+        var contextState = context.phase;
+        
+        if (contextState == InputActionPhase.Started)
+        {
+        }
+        else if (contextState == InputActionPhase.Performed)
+        {
+            if (_actionDict.TryGetValue(context.action, out var handler))
+            {
+                handler.Invoke(context);
+            }
+        }
+        else if (contextState == InputActionPhase.Canceled)
+        {
+        }
     }
 
-
+    private void OnHandleMove(InputAction.CallbackContext context)
+    {
+        Debug.Log("hi");
+    }
     #endregion
 
     #region 5. Request Methods
