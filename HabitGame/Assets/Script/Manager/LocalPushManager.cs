5983b4a53fea40d71ca11de2f8ba4948d2d7afb1
JIWON PARK <55792986+pjw960316@users.noreply.github.com>
2026-09-14 11:28:01 +0900

    feat. LocalPushManager 추가
    - LocalPushManager 클래스 생성 및 기본 구조 정의
    - ManagerBase<T> 상속 및 필드, 속성, 생성자, 이벤트 핸들러, 메서드 구분

---
+++ HabitGame/Assets/Script/Manager/LocalPushManager.cs
@@ -0,0 +1,32 @@
+public sealed class LocalPushManager : ManagerBase<LocalPushManager>
+{
+    #region 1. Fields
+
+    //
+
+    #endregion
+
+    #region 2. Properties
+
+    //
+
+    #endregion
+
+    #region 3. Constructor
+
+    //
+
+    #endregion
+
+    #region 4. EventHandlers
+
+    //
+
+    #endregion
+
+    #region 5. Methods
+
+    //
+
+    #endregion
+}
